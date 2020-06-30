6b22b3d7bd143251b3c26b24a837ad820fe21474
christopherdavi <67648870+christopherdavi@users.noreply.github.com>
2020-06-30 12:48:27 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primer_programa
+{
+    class Principal
+
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba su nombre: ");
+            string nombre = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Escriba su primer numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Escriba su segundo numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado;
+
+            resultado = num1 + num2;
+
+            Console.WriteLine("El resultado es " + nombre);
+            Console.WriteLine("El resultado es " + resultado);
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
+
+
